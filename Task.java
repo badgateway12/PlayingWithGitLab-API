8b2503bd2803f9a9dd81c591b063504aea817232
Irene Butakova <allegr12@gmail.com>
2019-02-21 20:49:32 +0200

    fixed comment format

--- Task.java
+++ Task.java
@@ -1,8 +1,8 @@
-/**
- * Given a string, find the first repeated character in it. Return null if no such character is found.
- * Example: "abca" = 'a', "awsd" = null, "abssb" = 's'
- * Assume, we have lowercase letters only.
- */
+/*
+ Given a string, find the first repeated character in it. Return null if no such character is found.
+ Example: "abca" = 'a', "awsd" = null, "abssb" = 's'
+ Assume, we have lowercase letters only.
+*/
 public class Task {
     public static Character firstRepeatingCharacter(String input)
     {
